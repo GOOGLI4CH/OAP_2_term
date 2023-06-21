61c0806aa69b4e94f870dcf81f085eab9c689080
GOOGLICH <Uglyanitsa_M@it.belstu.by>
2023-06-21 13:50:48 +0300

    laboratory work 15

---
+++ 15 Хеш-таблицы с цепочками/Source.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+#include "HashTable.h"
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int size;
+	cout << "Введите размер хеш-таблицы: "; cin >> size;
+	HashTable<apartments, int> table(size);
+	int choice = 0, num;
+	string str;
+
+	while (true)
+	{
+		cout << "Выберите вариант:" << endl;
+		cout << "1 - Добавить элемент в хеш-таблицу" << endl;
+		cout << "2 - Удалить элемент из хеш-таблицы" << endl;
+		cout << "3 - Поиск элемента в хеш-таблице" << endl;
+		cout << "4 - Вывод хеш-таблицы" << endl;
+		cout << "0 - Выход" << endl;
+		cin >> choice;
+
+		switch (choice)
+		{
+		case 1:
+		{
+			cout << "Введите номер квартиры: "; cin >> num;
+			cin.ignore();
+			cout << "Введите жильцов: "; getline(cin, str);
+			apartments apartment(num, str);
+			table.insert(apartment, num);
+			break;
+		}
+		case 2:
+		{
+			cout << "Введите номер квартиры, которая подлежит удалению: "; cin >> num;
+			table.remove(num);
+			break;
+		}
+		case 3:
+		{
+			cout << "Введите номер искомой квартиры: "; cin >> num;
+			cout << "Данные квартиры с номером " << num << ": " << endl;
+			table.printNode(table.search(num));
+			break;
+		}
+		case 4:
+		{
+			cout << "Хеш-таблица:" << endl;
+			table.print();
+			break;
+		}
+		case 0:
+		{
+			return 0;
+		}
+		default:
+			break;
+		}
+	}
+}
---
+++ 15 Хеш-таблицы с цепочками/TemplateHashTable.h
@@ -0,0 +1,56 @@
+﻿#pragma once
+#include <iostream>
+
+class apartments
+{
+public:
+	int apartment_number;
+	std::string lodgers;
+	apartments(int num = 0, std::string lodgers = "")
+	{
+		this->apartment_number = num;
+		this->lodgers = lodgers;
+	}
+};
+
+template<class Data, class Key>
+class HashTable
+{
+private:
+	class Node
+	{
+	public:
+		Data value;
+		Key key;
+		Node* next;
+		Node* prev;
+
+		Node(Data value, Key key, Node* next = nullptr, Node* prev = nullptr)
+		{
+			this->value = value;
+			this->key = key;
+			this->next = next;
+			this->prev = prev;
+		}
+	};
+
+	Node** table;
+	int size;
+	int capacity;
+
+	int hash(const Key& key) const
+	{
+		return static_cast<int>(key) % capacity;
+	}
+
+public:
+	HashTable(const int);
+	~HashTable();
+	void insert(const Data&, const Key&);
+	void printNode(Node*);
+	void remove(const Key&);
+	void print();
+	Node* search(const Key&);
+
+	friend std::ostream& operator<<(std::ostream& out, const apartments& value);
+};
